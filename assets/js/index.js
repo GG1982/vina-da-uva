2e5e9d70815f1295d9c60c78ed406ae988a03be6
GG1982 <giuliano.grasso26@gmail.com>
2022-05-19 01:20:51 +0100

    add navbar for mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,27 @@
-const navToggleButton = document.querySelector('.nav__toggle')
-const navList = document.querySelector('.nav__list')
+// const navToggleButton = document.querySelector('.nav__toggle')
+// const navList = document.querySelector('.nav__list')
 
 
-navToggleButton.addEventListener('click', () => {
-    navList.classList.toggle('active')
+// navToggleButton.addEventListener('click', () => {
+//     navList.classList.toggle('active')
+// })
+
+const nav = document.querySelector('.nav__list')
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('active');
 })
+
+// navToggle.addEventListener('click', () => {
+//     const visibility = nav.getAttribute('data-visible')
+//     if(visibility === "false") {
+//         nav.setAttribute('data-visible', true)
+//         navToggle.setAttribute('aria-expanded', true)
+//     } else if (visibility === "true") {
+//         nav.setAttribute('data-visible', false)
+//         navToggle.setAttribute('aria-expanded', false)
+//     }
+
+// })

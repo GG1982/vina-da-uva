a0762a038ee8f446fa1c2f0aa50998ec1e680860
GG1982 <giuliano.grasso26@gmail.com>
2022-05-19 02:26:15 +0100

    add js functionality and pixel size for media nav

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,14 +5,31 @@
 // navToggleButton.addEventListener('click', () => {
 //     navList.classList.toggle('active')
 // })
-
+const navContainer = document.querySelector('.nav__container')
 const nav = document.querySelector('.nav__list')
 const navToggle = document.querySelector('.mobile-nav-toggle')
 
+let menuOpen = false
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('active');
+    document.body.classList.toggle('active')
+
+    const visibility = nav.getAttribute('data-visible')
+    if (visibility === "false") {
+        nav.setAttribute('data-visible', true)
+        navToggle.setAttribute('aria-expanded', true)
+    } else {
+        nav.setAttribute('data-visible', false)
+        navToggle.setAttribute('aria-expanded', false)
+    }
 })
+
+// document.body.addEventListener('click', () => {
+//     console.log(nav)
+// })
+
+
+
 
 // navToggle.addEventListener('click', () => {
 //     const visibility = nav.getAttribute('data-visible')

86d770ae6286107b8e4554b3f2de6ead678c8e26
GG1982 <giuliano.grasso26@gmail.com>
2022-05-27 20:28:38 +0100

    chnage footer gap

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -3,7 +3,10 @@ const faqToggle = Array.from(document.querySelectorAll('.js-faq-title-toggle'))
 
 faqToggle.forEach(btn => {
     btn.addEventListener('click', () => {
+        // toggle faq-answers (open/close)
         btn.nextElementSibling.classList.toggle("faq-active")
+
+        // toggle +/- button
         btn.lastElementChild.classList.toggle("faq-active")
     })
 });

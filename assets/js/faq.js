b18e36782a25a2274250325d93210b399b045e08
GG1982 <giuliano.grasso26@gmail.com>
2022-05-27 20:12:38 +0100

    open and close faq menu on entire title

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -3,7 +3,7 @@ const faqToggle = Array.from(document.querySelectorAll('.js-faq-title-toggle'))
 
 faqToggle.forEach(btn => {
     btn.addEventListener('click', () => {
-        btn.parentElement.nextElementSibling.classList.toggle("faq-active")
-        btn.classList.toggle("faq-active")
+        btn.nextElementSibling.classList.toggle("faq-active")
+        btn.lastElementChild.classList.toggle("faq-active")
     })
 });

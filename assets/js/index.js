71a900ef526fc1cbc24a10ffd96a0a40d1bfe44f
GG1982 <giuliano.grasso26@gmail.com>
2022-05-27 16:32:20 +0100

    prevent scrolling when nav menu open

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,17 @@
-// const navToggleButton = document.querySelector('.nav__toggle')
-// const navList = document.querySelector('.nav__list')
 
-
-// navToggleButton.addEventListener('click', () => {
-//     navList.classList.toggle('active')
-// })
 const navContainer = document.querySelector('.nav__container')
 const navList = document.querySelector('.nav__list')
 const navToggle = document.querySelector('.mobile-nav-toggle')
 const hamburger = document.querySelector('.hamburger')
 const navLink = Array.from(document.querySelectorAll('.nav__link'))
+const body = document.querySelector('body')
 
 
 navToggle.addEventListener('click', () => {
+
+    // Prevent scrolling when menu open
+    body.classList.toggle('overflow-hidden')
+
     navContainer.classList.toggle('active')
 
     // Aria label. Tell sr if menu is open
@@ -29,12 +28,14 @@ navToggle.addEventListener('click', () => {
 navLink.forEach(link => {
     link.addEventListener('click', () => {
         navContainer.classList.toggle('active')
+        body.classList.toggle('overflow-hidden')
     })
 })
 
 document.body.addEventListener('click', (e) => {
     if (e.target !== navList && e.target !== navToggle && e.target !== hamburger) {
         navContainer.classList.remove('active')
+        body.classList.remove('overflow-hidden')
     }
 })
 

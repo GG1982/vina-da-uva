69e3dbf7f430070e186673a06d284df525999e6b
GG1982 <giuliano.grasso26@gmail.com>
2022-06-28 12:57:24 +0100

    add wine production calc

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-
+// Nav Bar For Mobile
 const navContainer = document.querySelector('.nav__container')
 const navList = document.querySelector('.nav__list')
 const navToggle = document.querySelector('.mobile-nav-toggle')
@@ -39,3 +39,35 @@ document.body.addEventListener('click', (e) => {
     }
 })
 
+
+
+// Wine Calculator
+let boxes = parseInt(document.getElementById('boxes').value)
+
+const notify = document.getElementById('notify')
+
+const liters = document.querySelector('.liters')
+const bottles = document.querySelector('.bottles')
+const weight = document.querySelector('.weight')
+
+
+function updateInput(input) {
+
+    if (input > -1 && input < 1001) {
+
+        notify.textContent = 'Select number of cases'
+
+        boxes = input
+
+        liters.textContent = `${boxes * 4.5}L`
+        bottles.textContent = `${boxes * 6}`
+        weight.textContent = `${boxes * 8.5}kg`
+    } else if (input > 1000) {
+        notify.textContent = "Please contact us for larger orders"
+    }
+    
+    else {
+        notify.textContent = "Please select a value bigger than 0"
+    }
+
+}

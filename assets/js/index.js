3fa897392ea54b6a1799e9691cd4461bde9955bf
GG1982 <giuliano.grasso26@gmail.com>
2022-06-29 12:49:47 +0100

    seperate src for calc.js and change site name

--- assets/js/index.js
+++ assets/js/index.js
@@ -38,36 +38,3 @@ document.body.addEventListener('click', (e) => {
         body.classList.remove('overflow-hidden')
     }
 })
-
-
-
-// Wine Calculator
-let boxes = parseInt(document.getElementById('boxes').value)
-
-const notify = document.getElementById('notify')
-
-const liters = document.querySelector('.liters')
-const bottles = document.querySelector('.bottles')
-const weight = document.querySelector('.weight')
-
-
-function updateInput(input) {
-
-    if (input > -1 && input < 1001) {
-
-        notify.textContent = 'Select number of cases'
-
-        boxes = input
-
-        liters.textContent = `${boxes * 4.5}L`
-        bottles.textContent = `${boxes * 6}`
-        weight.textContent = `${boxes * 8.5}kg`
-    } else if (input > 1000) {
-        notify.textContent = "Please contact us for larger orders"
-    }
-    
-    else {
-        notify.textContent = "Please select a value bigger than 0"
-    }
-
-}
